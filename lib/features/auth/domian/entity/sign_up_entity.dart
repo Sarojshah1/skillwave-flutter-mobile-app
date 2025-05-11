942daf571425222c7ad97d97de8962b10dad4f50
saroj kumar sah <sarojkumarsah@sarojs-MacBook-Air.local>
2025-05-11 18:44:29 +0545

    entity and models for auth login and signup

---
+++ lib/features/auth/domian/entity/sign_up_entity.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:equatable/equatable.dart';
+
+class SignUpEntity extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String role;
+  final String bio;
+
+  const SignUpEntity({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.role,
+    required this.bio,
+  });
+  @override
+  List<Object?> get props => [
+    name,
+    email,
+    password,
+    role,
+    bio,
+  ];
+}
