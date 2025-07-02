6a456bc251dbbb827829b5df4d91bc3493f5a7cc
saroj kumar sah <sarojahah152@hmail.com>
2025-07-02 21:32:22 +0545

    chore(android): update MainActivity.kt

--- android/app/src/main/kotlin/com/saroj/skillwave/skillwave/MainActivity.kt
+++ android/app/src/main/kotlin/com/saroj/skillwave/skillwave/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.saroj.skillwave.skillwave
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
