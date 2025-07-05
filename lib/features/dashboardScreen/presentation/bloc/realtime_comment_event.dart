65f505cc8ab31754d98774386241c81dbef8a2ac
saroj kumar sah <sarojahah152@hmail.com>
2025-07-05 13:11:47 +0545

    feat: add realtime comment events for bloc communication

---
+++ lib/features/dashboardScreen/presentation/bloc/realtime_comment_event.dart
@@ -0,0 +1,56 @@
+part of 'realtime_comment_bloc.dart';
+
+abstract class RealtimeCommentEvent extends Equatable {
+  const RealtimeCommentEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class InitializeSocket extends RealtimeCommentEvent {
+  final String baseUrl;
+  final String? token;
+
+  const InitializeSocket({this.baseUrl = ApiEndpoints.baseUrl, this.token});
+
+  @override
+  List<Object?> get props => [baseUrl, token];
+}
+
+class JoinPostRoom extends RealtimeCommentEvent {
+  final String postId;
+
+  const JoinPostRoom({required this.postId});
+
+  @override
+  List<Object> get props => [postId];
+}
+
+class LeavePostRoom extends RealtimeCommentEvent {
+  final String postId;
+
+  const LeavePostRoom({required this.postId});
+
+  @override
+  List<Object> get props => [postId];
+}
+
+class NewCommentReceived extends RealtimeCommentEvent {
+  final Map<String, dynamic> data;
+
+  const NewCommentReceived(this.data);
+
+  @override
+  List<Object> get props => [data];
+}
+
+class NewReplyReceived extends RealtimeCommentEvent {
+  final Map<String, dynamic> data;
+
+  const NewReplyReceived(this.data);
+
+  @override
+  List<Object> get props => [data];
+}
+
+class DisposeSocket extends RealtimeCommentEvent {}
