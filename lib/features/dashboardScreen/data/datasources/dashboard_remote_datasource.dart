906f9235e17cbaf32c43f1f28d294ef9c7af2bd1
saroj kumar sah <sarojahah152@hmail.com>
2025-07-05 09:03:52 +0545

    feat: Add dashboard remote data source with API endpoints

---
+++ lib/features/dashboardScreen/data/datasources/dashboard_remote_datasource.dart
@@ -0,0 +1,116 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:skillwave/config/constants/api_endpoints.dart';
+import 'package:skillwave/cores/failure/failure.dart';
+import '../models/post_model.dart';
+import '../models/post_dto.dart';
+import '../../domin/entity/post_entity.dart';
+
+@LazySingleton()
+class DashboardRemoteDatasource {
+  final Dio dio;
+  DashboardRemoteDatasource({required this.dio});
+
+  Future<PostsResponseEntity> getPosts(GetPostsDto dto) async {
+    try {
+      final response = await dio.get(
+        ApiEndpoints.getPost,
+        queryParameters: dto.toJson(),
+      );
+      return PostsResponseModel.fromJson(response.data).toEntity();
+    } on DioException catch (e) {
+      throw ApiFailure(message: e.message.toString());
+    } catch (e) {
+      throw ApiFailure(message: e.toString());
+    }
+  }
+
+  Future<PostEntity> getPostById(String id) async {
+    try {
+      final response = await dio.get("${ApiEndpoints.getPost}/$id");
+      return PostModel.fromJson(response.data).toEntity();
+    } on DioException catch (e) {
+      throw ApiFailure(message: e.message.toString());
+    } catch (e) {
+      throw ApiFailure(message: e.toString());
+    }
+  }
+
+  Future<PostEntity> createPost(CreatePostDto dto) async {
+    try {
+      final response = await dio.post(
+        ApiEndpoints.createPost,
+        data: dto.toJson(),
+      );
+      return PostModel.fromJson(response.data).toEntity();
+    } on DioException catch (e) {
+      throw ApiFailure(message: e.message.toString());
+    } catch (e) {
+      throw ApiFailure(message: e.toString());
+    }
+  }
+
+  Future<PostEntity> updatePost(String id, UpdatePostDto dto) async {
+    try {
+      final response = await dio.put(
+        "${ApiEndpoints.getPost}/$id",
+        data: dto.toJson(),
+      );
+      return PostModel.fromJson(response.data).toEntity();
+    } on DioException catch (e) {
+      throw ApiFailure(message: e.message.toString());
+    } catch (e) {
+      throw ApiFailure(message: e.toString());
+    }
+  }
+
+  Future<void> deletePost(String id) async {
+    try {
+      await dio.delete("${ApiEndpoints.getPost}/$id");
+    } on DioException catch (e) {
+      throw ApiFailure(message: e.message.toString());
+    } catch (e) {
+      throw ApiFailure(message: e.toString());
+    }
+  }
+
+  Future<void> likePost(String postId) async {
+    try {
+      await dio.post(ApiEndpoints.getpostlikeUrl(postId));
+    } on DioException catch (e) {
+      throw ApiFailure(message: e.message.toString());
+    } catch (e) {
+      throw ApiFailure(message: e.toString());
+    }
+  }
+
+  Future<void> createComment(String postId, CreateCommentDto dto) async {
+    try {
+      await dio.post(
+        ApiEndpoints.getpostcommentUrl(postId),
+        data: dto.toJson(),
+      );
+    } on DioException catch (e) {
+      throw ApiFailure(message: e.message.toString());
+    } catch (e) {
+      throw ApiFailure(message: e.toString());
+    }
+  }
+
+  Future<void> createReply(
+    String postId,
+    String commentId,
+    CreateReplyDto dto,
+  ) async {
+    try {
+      await dio.post(
+        ApiEndpoints.getpostcommentReplyUrl(postId, commentId),
+        data: dto.toJson(),
+      );
+    } on DioException catch (e) {
+      throw ApiFailure(message: e.message.toString());
+    } catch (e) {
+      throw ApiFailure(message: e.toString());
+    }
+  }
+}
