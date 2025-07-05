cc2a5c1c990055b95781165da79d04173b33aa89
saroj kumar sah <sarojahah152@hmail.com>
2025-07-05 09:06:16 +0545

    feat: Add ChangePassword BLoC for password change functionality

---
+++ lib/features/profileScreen/presentation/bloc/change_password_bloc/change_password_events.dart
@@ -0,0 +1,23 @@
+part of 'change_password_bloc.dart';
+
+abstract class ChangePasswordEvent extends Equatable {
+  const ChangePasswordEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class ChangePasswordRequested extends ChangePasswordEvent {
+  final String currentPassword;
+  final String newPassword;
+  final String confirmPassword;
+
+  const ChangePasswordRequested({
+    required this.currentPassword,
+    required this.newPassword,
+    required this.confirmPassword,
+  });
+
+  @override
+  List<Object?> get props => [currentPassword, newPassword, confirmPassword];
+}
