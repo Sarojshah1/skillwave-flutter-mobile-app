24a00a07ea4cbec291fd3f0a057d944e7359c2a7
saroj kumar sah <sarojahah152@hmail.com>
2025-07-10 19:40:35 +0545

    Update dark_theme.dart

--- lib/config/themes/ theme/dark_theme.dart
+++ lib/config/themes/ theme/dark_theme.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:skillwave/config/themes/app_themes_color.dart';
 import '../styles/text_theme.dart';
 import '../styles/input_theme.dart';
-import '../styles/button_theme.dart';
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
